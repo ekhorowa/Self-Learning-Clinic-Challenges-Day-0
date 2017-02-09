9a97c44c646e2f41c31df6f14037e656f4b423b0
Omokaro Faith <ekhorowaekhorowa@gmail.com>
2017-02-09 06:46:32 +0100

    update minMax program

--- minMax/apps/minMax.js
+++ minMax/apps/minMax.js
@@ -1,10 +1,11 @@
+// This program returns the Minimumm and Maximum number in an array
 module.exports = {
-min_max: function (d_array) {
-  var minimum = Math.min.apply(null, d_array); 
-  var maximum = Math.max.apply(null, d_array);
-  if (minimum === maximum) {
-  	return [minimum]
-  }
-  return [minimum, maximum]
-}
+	min_max: function (d_array) {
+  	var minimum = Math.min.apply(null, d_array); 
+  	var maximum = Math.max.apply(null, d_array);
+  	if (minimum === maximum) {
+  		return [minimum]
+  	}
+  	return [minimum, maximum]
+	}
 }
