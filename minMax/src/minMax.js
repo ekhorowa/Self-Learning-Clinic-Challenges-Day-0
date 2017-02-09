e965e3ea2cddafe8e37fb637c11809eab94444ca
Omokaro Faith <ekhorowaekhorowa@gmail.com>
2017-02-09 08:41:22 +0100

    change made to minMax/src file

---
+++ minMax/src/minMax.js
@@ -0,0 +1,11 @@
+// This program returns the Minimumm and Maximum number in an array
+	function min_max (d_array) {
+  	var minimum = Math.min.apply(null, d_array); 
+  	var maximum = Math.max.apply(null, d_array);
+  	if (minimum === maximum) {
+  		return [minimum]
+  	}
+  	return [minimum, maximum]
+	}
+
+module.exports = min_max;
