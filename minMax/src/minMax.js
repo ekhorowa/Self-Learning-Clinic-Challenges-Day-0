a40d823a134a0d37b9470c73087f3a14d24c3050
Omokaro Faith <ekhorowaekhorowa@gmail.com>
2017-02-09 09:57:33 +0100

    more test cases added

--- minMax/src/minMax.js
+++ minMax/src/minMax.js
@@ -1,5 +1,6 @@
 // This program returns the Minimumm and Maximum number in an array
 	function min_max (d_array) {
+	//variable declaration
   	var minimum = Math.min.apply(null, d_array); 
   	var maximum = Math.max.apply(null, d_array);
   	if (minimum === maximum) {
